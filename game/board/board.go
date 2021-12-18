93e66d55416ecca4381ac7ff8647f2db48bcd898
khjj07 <khjj07@gmail.com>
2021-12-19 03:18:55 +0900

    sound

--- game/board/board.go
+++ game/board/board.go
@@ -31,3 +31,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "match"
+  type: "sound"
+  data: "sound: \"/assets/Sound/MatchComplete_Sfx.wav\"\n"
+  "looping: 0\n"
+  "group: \"sfx\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- game/manager/manager.go
+++ game/manager/manager.go
@@ -13,3 +13,95 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "ingame_bgm1"
+  type: "sound"
+  data: "sound: \"/assets/Sound/GameSong1_Bgm.wav\"\n"
+  "looping: 0\n"
+  "group: \"bgm\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ingame_bgm2"
+  type: "sound"
+  data: "sound: \"/assets/Sound/GameSong2_Bgm.wav\"\n"
+  "looping: 0\n"
+  "group: \"bgm\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ingame_bgm3"
+  type: "sound"
+  data: "sound: \"/assets/Sound/GameSong3_Bgm.wav\"\n"
+  "looping: 0\n"
+  "group: \"bgm\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "fever_bgm"
+  type: "sound"
+  data: "sound: \"/assets/Sound/FeverSong_Bgm.wav\"\n"
+  "looping: 0\n"
+  "group: \"bgm\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
