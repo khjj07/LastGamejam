c6a59373f5a90f9abb8bf2c4cea53d256c7e74ff
khjj07 <khjj07@gmail.com>
2021-12-18 21:35:31 +0900

    매칭 구현

--- game/enemy/enemy13.go
+++ game/enemy/enemy13.go
@@ -13,6 +13,31 @@ components {
     w: 1.0
   }
   properties {
+    id: "speed"
+    value: "75.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "hp"
+    value: "20.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "maxhp"
+    value: "40.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "tag1"
+    value: "human"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "tag2"
+    value: "sword"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
     id: "fliped_sprite"
     value: "true"
     type: PROPERTY_TYPE_BOOLEAN
@@ -95,6 +120,55 @@ embedded_components {
     x: -17.0
     y: 38.0
     z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"enemy\"\n"
+  "mask: \"deadline\"\n"
+  "mask: \"startline\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 10.0\n"
+  "  data: 10.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
   }
   rotation {
     x: 0.0
